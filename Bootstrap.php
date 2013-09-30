9e5108ad116e05d39a3faee3ceac5f7dfae32485
Daniel Bächtle <daniel.baechtle@plentymarkets.com>
2013-09-30 16:09:22 +0200

    UPDATE Install (set version)

--- Bootstrap.php
+++ Bootstrap.php
@@ -78,6 +78,9 @@ class Shopware_Plugins_Backend_PlentyConnector_Bootstrap extends Shopware_Compon
         $this->createMenu();
         $this->registerCronjobs();
         
+        //
+        PlentymarketsConfig::getInstance()->setConnectorVersion($this->getVersion());
+        
         return true;
     }
     
