4fc2a4b3da25c5e2e6f45881bb215c9cec9bb560
Daniel Bächtle <daniel.baechtle@plentymarkets.com>
2013-11-12 16:11:35 +0100

    UPDATE Attributes (last update timestamp)

---
+++ Components/Import/Controller/PlentymarketsImportControllerItemAttribute.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once PY_SOAP . 'Models/PlentySoapRequest/GetItemAttributes.php';
+require_once PY_COMPONENTS . 'Import/Entity/PlentymarketsImportEntityItemAttribute.php';
+
+/**
+ * Imports the item producers
+ *
+ * @author Daniel Bächtle <daniel.baechtle@plentymarkets.com>
+ */
+class PlentymarketsImportControllerItemAttribute
+{
+	/**
+	 * Performs the actual import
+	 */
+	public function run($lastUpdateTimestamp)
+	{
+
+		$Request_GetItemAttributes = new PlentySoapRequest_GetItemAttributes();
+		$Request_GetItemAttributes->GetValues = true;
+		$Request_GetItemAttributes->LastUpdateFrom = $lastUpdateTimestamp;
+
+		$Response_GetItemAttributes = PlentymarketsSoapClient::getInstance()->GetItemAttributes($Request_GetItemAttributes);
+		$Response_GetItemAttributes instanceof PlentySoapResponse_GetItemAttributes;
+
+		foreach ($Response_GetItemAttributes->Attributes->item as $Attribute)
+		{
+			$PlentymarketsImportEntityItemAttribute = new PlentymarketsImportEntityItemAttribute($Attribute);
+			$PlentymarketsImportEntityItemAttribute->import();
+		}
+	}
+}
+
