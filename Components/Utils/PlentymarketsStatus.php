7451d55672f8d5eba841ee4934ac392348a94877
Daniel Bächtle <daniel.baechtle@plentymarkets.com>
2013-11-22 15:33:12 +0100

    UPDATE Satus

--- Components/Utils/PlentymarketsStatus.php
+++ Components/Utils/PlentymarketsStatus.php
@@ -1,4 +1,30 @@
 <?php
+/**
+ * plentymarkets shopware connector
+ * Copyright © 2013 plentymarkets GmbH
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License, supplemented by an additional
+ * permission, and of our proprietary license can be found
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "plentymarkets" is a registered trademark of plentymarkets GmbH.
+ * "shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, titles and interests in the
+ * above trademarks remain entirely with the trademark owners.
+ *
+ * @copyright  Copyright (c) 2013, plentymarkets GmbH (http://www.plentymarkets.com)
+ * @author     Daniel Bächtle <daniel.baechtle@plentymarkets.com>
+ */
 
 require_once PY_SOAP . 'Client/PlentymarketsSoapClient.php';
 require_once PY_COMPONENTS . 'Config/PlentymarketsConfig.php';
@@ -6,21 +32,54 @@ require_once PY_COMPONENTS . 'Mapping/PlentymarketsMappingController.php';
 require_once PY_COMPONENTS . 'Export/PlentymarketsExportController.php';
 require_once PY_COMPONENTS . 'Utils/DataIntegrity/PlentymarketsDataIntegrityController.php';
 
+/**
+ * Determines the status of the plugin regarding to the communication with plenty
+ *
+ * @author Daniel Bächtle <daniel.baechtle@plentymarkets.com>
+ */
 class PlentymarketsStatus
 {
-
+	/**
+	 *
+	 * @var PlentymarketsStatus
+	 */
 	protected static $Instance;
 
+	/**
+	 *
+	 * @var boolean
+	 */
 	protected $isConnected = false;
 
+	/**
+	 *
+	 * @var integer
+	 */
 	protected $connectionTimestamp = 0;
 
+	/**
+	 *
+	 * @var boolean
+	 */
 	protected $isCliWarningLogged = false;
 
+	/**
+	 *
+	 * @var boolean
+	 */
 	protected $isRuntimeWarningLogged = false;
 
+	/**
+	 *
+	 * @var boolean
+	 */
 	protected $isLicenseWarningLogged = false;
 
+	/**
+	 * I am the singleton method
+	 *
+	 * @return PlentymarketsStatus
+	 */
 	public static function getInstance()
 	{
 		if (!self::$Instance instanceof self)
@@ -30,6 +89,11 @@ class PlentymarketsStatus
 		return self::$Instance;
 	}
 
+	/**
+	 * Checks whether the connection to plentymarkets can be established
+	 *
+	 * @return boolean
+	 */
 	public function isConnected()
 	{
 		// The connection is only checked every 10 seconds
@@ -76,6 +140,11 @@ class PlentymarketsStatus
 		}
 	}
 
+	/**
+	 * Checks whether the settings are completely done
+	 *
+	 * @return boolean
+	 */
 	protected function isSettingsFinished()
 	{
 		$isSettingsFinished = PlentymarketsConfig::getInstance()->isComplete();
@@ -83,6 +152,11 @@ class PlentymarketsStatus
 		return $isSettingsFinished;
 	}
 
+	/**
+	 * Checks whether the mappings are completely done
+	 *
+	 * @return boolean
+	 */
 	protected function isMappingFinished()
 	{
 		$isMappingFinished = PlentymarketsMappingController::isComplete();
@@ -90,6 +164,11 @@ class PlentymarketsStatus
 		return $isMappingFinished;
 	}
 
+	/**
+	 * Checks whether the initial exports are completely done
+	 *
+	 * @return boolean
+	 */
 	protected function isExportFinished()
 	{
 		$isExportFinished = PlentymarketsExportController::getInstance()->isComplete();
@@ -97,6 +176,11 @@ class PlentymarketsStatus
 		return $isExportFinished;
 	}
 
+	/**
+	 * Checks whether the data integerity is valid
+	 *
+	 * @return boolean
+	 */
 	protected function isDataIntegrityValid()
 	{
 		$isDataIntegrityValid = PlentymarketsDataIntegrityController::getInstance()->isValid();;
@@ -104,11 +188,21 @@ class PlentymarketsStatus
 		return $isDataIntegrityValid;
 	}
 
+	/**
+	 * Checks whether data may be imported
+	 *
+	 * @return boolean
+	 */
 	public function mayImport()
 	{
 		return $this->isConnected();
 	}
 
+	/**
+	 * Checks whether data may be exported
+	 *
+	 * @return boolean
+	 */
 	public function mayExport()
 	{
 		return (
@@ -126,6 +220,12 @@ class PlentymarketsStatus
 		);
 	}
 
+	/**
+	 * Checks whether data may be synchronized
+	 *
+	 * @param boolean $checkExtended Perform the extended checks (memory and runtime)
+	 * @return boolean
+	 */
 	public function maySynchronize($checkExtended=true)
 	{
 		// Export has basically the same needs
@@ -134,29 +234,32 @@ class PlentymarketsStatus
 		// Export is okay
 		$isExportFinished = $this->isExportFinished();
 
-// 		// Check the license
-// 		if (Shopware()->Bootstrap()->issetResource('License'))
-// 		{
-// 			$License = Shopware()->License();
-// 			$isLicenseValid = $License->checkCoreLicense(false);
-// 			if (!$isLicenseValid && !$this->isLicenseWarningLogged)
-// 			{
-// 				PlentymarketsLogger::getInstance()->error('System:License', 'The shopware license that is used is invalid or has expired.', 1010);
-// 				$this->isLicenseWarningLogged = true;
-// 			}
-// 		}
-// 		else
-// 		{
-// 			$isLicenseValid = false;
-// 			if (!$this->isLicenseWarningLogged)
-// 			{
-// 				PlentymarketsLogger::getInstance()->error('System:License', 'The license mananger is not installed. Therefore, it is not possible to check the license.', 1011);
-// 				$this->isLicenseWarningLogged = true;
-// 			}
-// 		}
+		// Check the license
+		if (Shopware()->Bootstrap()->issetResource('License'))
+		{
+			$License = Shopware()->License();
+			$isLicenseValid = $License->checkCoreLicense(false);
+			if (!$isLicenseValid && !$this->isLicenseWarningLogged)
+			{
+				PlentymarketsLogger::getInstance()->error('System:License', 'The shopware license that is used is invalid or has expired.', 1010);
+				$this->isLicenseWarningLogged = true;
+			}
+		}
+		else
+		{
+			$isLicenseValid = false;
+			if (!$this->isLicenseWarningLogged)
+			{
+				PlentymarketsLogger::getInstance()->error('System:License', 'The license mananger is not installed. Therefore, it is not possible to check the license.', 1011);
+				$this->isLicenseWarningLogged = true;
+			}
+		}
+
+		//
+		$isLicenseValid = true;
 
 		// May Synchronize
-		$maySynchronize = $mayExport && $isExportFinished/* && $isLicenseValid*/;
+		$maySynchronize = $mayExport && $isExportFinished && $isLicenseValid;
 
 		// User settings
 		$mayDatexActual = PlentymarketsConfig::getInstance()->getMayDatexUser(0);
@@ -185,23 +288,25 @@ class PlentymarketsStatus
 		if ($checkExtended && !isset($_GET['overruleExtendedChecks']))
 		{
 			// Check the cli
-			if (php_sapi_name() != 'cli')
+			$sapi = php_sapi_name();
+			if ($sapi != 'cli')
 			{
 				$isCli = false;
 				if (!$this->isCliWarningLogged)
 				{
-					PlentymarketsLogger::getInstance()->error('System:PHP', 'The synchronizing processes have to be started with the PHP-CLI (command line interface).', 1001);
+					PlentymarketsLogger::getInstance()->error('System:PHP', 'The synchronizing processes have to be started with the PHP-CLI (command line interface). You are using »' . $sapi . '«.', 1001);
 					$this->isCliWarningLogged = true;
 				}
 			}
 
 			// Check the runtime
-			if (ini_get('max_execution_time') > 0)
+			$runtime = ini_get('max_execution_time');
+			if ($runtime > 0)
 			{
 				$mayRunUnlimited = false;
 				if (!$this->isRuntimeWarningLogged)
 				{
-					PlentymarketsLogger::getInstance()->error('System:PHP', 'The synchronizing processes have to be started with unlimited runtime.', 1002);
+					PlentymarketsLogger::getInstance()->error('System:PHP', 'The synchronizing processes have to be started with unlimited runtime. Your runtime is limited to »' . $runtime . '« seconds.', 1002);
 					$this->isRuntimeWarningLogged = true;
 				}
 			}
