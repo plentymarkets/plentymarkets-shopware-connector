f858853a58141e6d1d31873d5e95215cf71c7063
Daniel Bächtle <daniel.baechtle@plentymarkets.com>
2013-11-26 09:59:13 +0100

    UPDATE Docstrings

--- Components/Soap/Models/PlentySoapRequest/GetProducers.php
+++ Components/Soap/Models/PlentySoapRequest/GetProducers.php
@@ -1,19 +1,43 @@
 <?php
+/**
+ * plentymarkets shopware connector
+ * Copyright © 2013 plentymarkets GmbH
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License, supplemented by an additional
+ * permission, and of our proprietary license can be found
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "plentymarkets" is a registered trademark of plentymarkets GmbH.
+ * "shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, titles and interests in the
+ * above trademarks remain entirely with the trademark owners.
+ *
+ * @copyright  Copyright (c) 2013, plentymarkets GmbH (http://www.plentymarkets.com)
+ * @author     Daniel Bächtle <daniel.baechtle@plentymarkets.com>
+ */
+
 
 /**
- * @date Thu, 17 Oct 2013 01:06:17 +0200
- * @gitrev 5f1b731d9d68773b2a8d43e289900c12ec2617eb
- * @author /workspace/py/unit_test/modules/api/soap/generator/templates/Model.tpl.php
- * @author generate.cli.php
+ * I am a generated class and am required for communicating with plentymarkets.
  */
 class PlentySoapRequest_GetProducers
 {
-	
+
 	/**
 	 * @var int
 	 */
 	public $LastUpdateFrom;
-	
+
 	/**
 	 * @var int
 	 */
