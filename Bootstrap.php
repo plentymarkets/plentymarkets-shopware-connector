47f9b259c7c6f20aba9046477b636a4b7ef8a0ea
Daniel Bächtle <daniel.baechtle@plentymarkets.com>
2015-04-27 15:34:38 +0200

    Version 1.7.1

--- Bootstrap.php
+++ Bootstrap.php
@@ -1246,7 +1246,7 @@ class Shopware_Plugins_Backend_PlentyConnector_Bootstrap extends Shopware_Compon
      */
     public function getVersion()
     {
-    	return '1.7.0';
+    	return '1.7.1';
     }
 
     /**
