08d460e9388c827106302943d0959d2808706f9c
Florian Ressel <info@k-30.de>
2016-03-29 15:19:23 +0200

    item import - setting the additionaltext for a variant
    to set the addtionaltext for a variant is deprecated since shopware version >= 5.0.0

--- Components/Import/Entity/PlentymarketsImportEntityItem.php
+++ Components/Import/Entity/PlentymarketsImportEntityItem.php
@@ -451,7 +451,11 @@ class PlentymarketsImportEntityItem
 				$details['shippingtime'] = $shippingTime;
 			}
 
-			$details['additionaltext'] = $AttributeValueSet->AttributeValueSetName;
+			if(version_compare(Shopware::VERSION, '4.3.6', '<='))
+			{
+				$details['additionaltext'] = $AttributeValueSet->AttributeValueSetName;
+			}
+			
 			$details['ean'] = $AttributeValueSet->EAN;
 			$details['X_plentySku'] = $sku;
 
