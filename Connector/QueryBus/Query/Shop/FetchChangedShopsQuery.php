4b14ebf36595c5bf9b1ffb79830163819f2a27ac
jochenmanz <jochen.manz@gmx.de>
2016-12-13 17:35:48 +0100

    added the shop querys for the later created mapping of shops

---
+++ Connector/QueryBus/Query/Shop/FetchChangedShopsQuery.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace PlentyConnector\Connector\QueryBus\Query\Shop;
+
+use PlentyConnector\Connector\QueryBus\Query\FetchChangedQueryInterface;
+
+/**
+ * Class FetchChangedShopsQuery.
+ */
+class FetchChangedShopsQuery implements FetchChangedQueryInterface
+{
+    /**
+     * @var string
+     */
+    private $adapterName;
+
+    /**
+     * FetchChangedShopsQuery constructor.
+     *
+     * @param string $adapterName
+     */
+    public function __construct($adapterName)
+    {
+        $this->adapterName = $adapterName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdapterName()
+    {
+        return $this->adapterName;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPayload()
+    {
+        return [
+            'adapterName' => $this->adapterName,
+        ];
+    }
+
+    /**
+     * @param array $payload
+     */
+    public function setPayload(array $payload = [])
+    {
+        $this->adapterName = $payload['adapterName'];
+    }
+}
---
+++ Connector/QueryBus/Query/Shop/FetchShopQuery.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace PlentyConnector\Connector\QueryBus\Query\Shop;
+
+use Assert\Assertion;
+use PlentyConnector\Connector\QueryBus\Query\FetchQueryInterface;
+
+/**
+ * Class FetchShopQuery
+ */
+class FetchShopQuery implements FetchQueryInterface
+{
+    /**
+     * @var string
+     */
+    private $adapterName;
+
+    /**
+     * @var string
+     */
+    private $identifier;
+
+    /**
+     * FetchShopQuery constructor.
+     *
+     * @param string $adapterName
+     * @param $identifier
+     */
+    public function __construct($adapterName, $identifier)
+    {
+        Assertion::uuid($identifier);
+
+        $this->adapterName = $adapterName;
+        $this->identifier = $identifier;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAdapterName()
+    {
+        return $this->adapterName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdentifier()
+    {
+        return $this->identifier;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPayload()
+    {
+        return [
+            'adapterName' => $this->adapterName,
+            'identifier' => $this->identifier,
+        ];
+    }
+
+    /**
+     * @param array $payload
+     */
+    public function setPayload(array $payload = [])
+    {
+        $this->adapterName = $payload['adapterName'];
+        $this->identifier = $payload['identifier'];
+    }
+}
