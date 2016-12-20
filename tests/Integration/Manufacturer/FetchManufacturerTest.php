a34efe3532cba320dad669653c3b4fd947e2b38d
jochenmanz <jochen.manz@gmx.de>
2016-12-20 09:23:26 +0100

    fixed the unit tests

---
+++ tests/Integration/Manufacturer/FetchManufacturerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PlentyConnector\Tests\Integration\Manufacturer;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class FetchManufacturerTest
+ */
+class FetchManufacturerTest extends TestCase
+{
+    /**
+     *
+     */
+    public function test_fetch_all_manufacturers()
+    {
+
+    }
+}
