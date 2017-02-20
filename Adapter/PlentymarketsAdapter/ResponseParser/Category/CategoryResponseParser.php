b34566ac8dc6b4a08299843f79071965e45f58bd
Jochen Manz <jochen.manz@gmx.de>
2017-02-20 13:39:10 +0100

    fixed the category parser

--- Adapter/PlentymarketsAdapter/ResponseParser/Category/CategoryResponseParser.php
+++ Adapter/PlentymarketsAdapter/ResponseParser/Category/CategoryResponseParser.php
@@ -56,6 +56,10 @@ class CategoryResponseParser implements CategoryResponseParserInterface
     {
         $result = [];
 
+        if (empty($entry['plentyId'])) {
+            return null;
+        }
+
         $categoryIdentifier = $this->identityService->findOneOrCreate(
             (string) ($entry['plentyId'] . '-' . $entry['categoryId']),
             PlentymarketsAdapter::NAME,
