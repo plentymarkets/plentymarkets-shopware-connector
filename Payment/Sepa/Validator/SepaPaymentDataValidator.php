e193ce58f4e085cfb3422845d2d827559783a246
Jochen Manz <jochen.manz@gmx.de>
2017-04-20 15:40:54 +0200

    update validator names

--- Payment/Sepa/Validator/SepaPaymentDataValidator.php
+++ Payment/Sepa/Validator/SepaPaymentDataValidator.php
@@ -24,10 +24,10 @@ class SepaPaymentDataValidator implements ValidatorInterface
      */
     public function validate($object)
     {
-        Assertion::string($object->getAccountOwner(), null, 'paymentData.accountOwner');
-        Assertion::notBlank($object->getAccountOwner(), null, 'paymentData.accountOwner');
-        Assertion::string($object->getIban(), null, 'paymentData.iban');
-        Assertion::notBlank($object->getIban(), null, 'paymentData.iban');
-        Assertion::nullOrString($object->getBic(), null, 'paymentData.bic');
+        Assertion::string($object->getAccountOwner(), null, 'payment.sepa.accountOwner');
+        Assertion::notBlank($object->getAccountOwner(), null, 'payment.sepa.accountOwner');
+        Assertion::string($object->getIban(), null, 'payment.sepa.iban');
+        Assertion::notBlank($object->getIban(), null, 'payment.sepa.iban');
+        Assertion::nullOrString($object->getBic(), null, 'payment.sepa.bic');
     }
 }
