a8bc8cf66bea55fbe4c1eded8d951b8a31cf673d
Jochen Manz <jochen.manz@gmx.de>
2017-05-30 11:45:29 +0200

    add new field to attribute function

--- Adapter/ShopwareAdapter/Helper/AttributeHelper.php
+++ Adapter/ShopwareAdapter/Helper/AttributeHelper.php
@@ -3,6 +3,9 @@
 namespace PlentyConnector\Adapter\ShopwareAdapter\Helper;
 
 use Assert\Assertion;
+use PlentyConnector\Connector\TransferObject\AttributableInterface;
+use PlentyConnector\Connector\TransferObject\TranslateableInterface;
+use PlentyConnector\Connector\ValidatorService\ValidatorServiceInterface;
 use PlentyConnector\Connector\ValueObject\Attribute\Attribute;
 use Shopware\Bundle\AttributeBundle\Service\CrudService;
 use Shopware\Bundle\AttributeBundle\Service\DataPersister;
@@ -30,6 +33,11 @@ class AttributeHelper
     private $dataPersister;
 
     /**
+     * @var ValidatorServiceInterface
+     */
+    private $validator;
+
+    /**
      * @var string
      */
     private $prefix = 'plenty_connector_';
@@ -37,18 +45,21 @@ class AttributeHelper
     /**
      * AttributeHelper constructor.
      *
-     * @param CrudService   $attributeService
-     * @param ModelManager  $entityManager
+     * @param CrudService $attributeService
+     * @param ModelManager $entityManager
      * @param DataPersister $dataPersister
+     * @param ValidatorServiceInterface $validator
      */
     public function __construct(
         CrudService $attributeService,
         ModelManager $entityManager,
-        DataPersister $dataPersister
+        DataPersister $dataPersister,
+        ValidatorServiceInterface $validator
     ) {
         $this->attributeService = $attributeService;
         $this->entityManager = $entityManager;
         $this->dataPersister = $dataPersister;
+        $this->validator = $validator;
     }
 
     /**
@@ -137,5 +148,52 @@ class AttributeHelper
         }
 
         return $result;
+    }
+
+    /**
+     * @param AttributableInterface $object
+     * @param $fieldName
+     */
+    public function addFieldAsAttribute(AttributableInterface $object, $fieldName)
+    {
+        if (!method_exists($object, 'getAttributes')) {
+            return;
+        }
+
+        $method = 'get' . ucfirst($fieldName);
+
+        if (!method_exists($object, $method)) {
+            return;
+        }
+
+        $fieldValue = $object->$method();
+
+        if (null === $fieldValue) {
+            return;
+        }
+
+        $attribute = new Attribute();
+        $attribute->setKey($fieldName);
+        $attribute->setValue($fieldValue);
+
+        if ($object instanceof TranslateableInterface) {
+            $translations = $object->getTranslations();
+            $newTranslations = [];
+
+            foreach ($translations as $translation) {
+                if ($fieldName === $translation->getProperty()) {
+                    $newTranslation = clone $translation;
+                    $newTranslation->setProperty('value');
+
+                    $newTranslations[] = $newTranslation;
+                }
+            }
+
+            $attribute->setTranslations($newTranslations);
+        }
+
+        $this->validator->validate($attribute);
+
+        $object->setAttributes(array_merge($object->getAttributes(), [$attribute]));
     }
 }
