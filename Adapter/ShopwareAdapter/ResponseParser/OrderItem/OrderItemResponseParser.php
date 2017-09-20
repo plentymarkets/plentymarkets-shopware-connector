4474f049eabc40dd436de762a4fb8dce9d947bc0
Jochen Manz <jochen.manz@gmx.de>
2017-09-21 00:52:58 +0200

    refactor order response parser

--- Adapter/ShopwareAdapter/ResponseParser/OrderItem/OrderItemResponseParser.php
+++ Adapter/ShopwareAdapter/ResponseParser/OrderItem/OrderItemResponseParser.php
@@ -7,7 +7,6 @@ use PlentyConnector\Connector\IdentityService\IdentityServiceInterface;
 use PlentyConnector\Connector\TransferObject\Order\OrderItem\OrderItem;
 use PlentyConnector\Connector\TransferObject\VatRate\VatRate;
 use ShopwareAdapter\ResponseParser\GetAttributeTrait;
-use ShopwareAdapter\ResponseParser\OrderItem\Exception\UnsupportedVatRateException;
 use ShopwareAdapter\ShopwareAdapter;
 
 /**
@@ -57,7 +56,7 @@ class OrderItemResponseParser implements OrderItemResponseParserInterface
     /**
      * @param array $entry
      *
-     * @throws UnsupportedVatRateException
+     * @throws NotFoundException
      *
      * @return null|string
      */
