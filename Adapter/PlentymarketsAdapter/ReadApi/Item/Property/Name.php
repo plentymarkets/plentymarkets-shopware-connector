ee54e9af673c05b1786e18fb75fa749c04e25bd4
Florian Ressel <info@k-30.de>
2018-09-12 13:13:55 +0200

    add item properties with value type "empty" (#341)
    * +1
    
    * add item properties with value type "empty"
    
    * add info into changelog
    
    * fix code style
    
    * fix changelog
    
    * fix cs
    
    * fix cs
    
    * fix cs
    
    * Revert "fix cs"
    
    This reverts commit a99720a9a269fba35936dcf43ded5f54ee1808b1.
    
    * fix cs
    
    * change service.xml spacing

---
+++ Adapter/PlentymarketsAdapter/ReadApi/Item/Property/Name.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PlentymarketsAdapter\ReadApi\Item\Property;
+
+use PlentymarketsAdapter\ReadApi\ApiAbstract;
+
+/**
+ * Class Name
+ */
+class Name extends ApiAbstract
+{
+    /**
+     * @param int $propertyId
+     *
+     * @return array
+     */
+    public function findOne($propertyId)
+    {
+        return $this->client->request('GET', 'items/properties/' . $propertyId . '/names');
+    }
+}
