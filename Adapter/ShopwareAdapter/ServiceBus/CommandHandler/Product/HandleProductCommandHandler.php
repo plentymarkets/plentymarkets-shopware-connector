d7f1ffb5ec01e687b6d2bc8a871171c38eec124d
Patryk Fabeck <Patryk.Fabeck@gmail.com>
2018-10-01 13:20:44 +0200

    bug fix (#380)
    * bug fix
    
    * styleCI

--- Adapter/ShopwareAdapter/ServiceBus/CommandHandler/Product/HandleProductCommandHandler.php
+++ Adapter/ShopwareAdapter/ServiceBus/CommandHandler/Product/HandleProductCommandHandler.php
@@ -15,6 +15,7 @@ use Shopware\Components\Api\Exception\NotFoundException;
 use Shopware\Components\Api\Manager;
 use Shopware\Components\Api\Resource\Article;
 use Shopware\Components\Api\Resource\Variant;
+use Shopware\Models\Article\Article as ArticleModel;
 use Shopware\Models\Article\Detail;
 use ShopwareAdapter\DataPersister\Attribute\AttributeDataPersisterInterface;
 use ShopwareAdapter\DataPersister\Translation\TranslationDataPersisterInterface;
@@ -157,7 +158,9 @@ class HandleProductCommandHandler implements CommandHandlerInterface
             );
         } else {
             try {
-                $productModel = $articleResource->getOne($productIdentity->getAdapterIdentifier());
+                $articleRepository = $this->entityManager->getRepository(ArticleModel::class);
+
+                $productModel = $articleRepository->find($productIdentity->getAdapterIdentifier());
 
                 if (null === $mainVariation) {
                     $variationResource = $this->getVariationResource();
@@ -171,7 +174,6 @@ class HandleProductCommandHandler implements CommandHandlerInterface
 
                 $this->correctMainDetailAssignment($mainVariation);
                 $productModel = $articleResource->update($productModel->getId(), $params);
-
             } catch (NotFoundException $exception) {
                 $productModel = $articleResource->create($params);
 
