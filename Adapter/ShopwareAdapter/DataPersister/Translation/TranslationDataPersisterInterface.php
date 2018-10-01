0e86a2a001a41bd15232bc5d1f7b0b2f25d40c53
Jochen Manz <jochen.manz@gmx.de>
2018-10-01 23:40:35 +0200

    shopware 5.5 category translation basics

--- Adapter/ShopwareAdapter/DataPersister/Translation/TranslationDataPersisterInterface.php
+++ Adapter/ShopwareAdapter/DataPersister/Translation/TranslationDataPersisterInterface.php
@@ -2,6 +2,7 @@
 
 namespace ShopwareAdapter\DataPersister\Translation;
 
+use PlentyConnector\Connector\TransferObject\Category\Category;
 use PlentyConnector\Connector\TransferObject\Product\Product;
 
 interface TranslationDataPersisterInterface
@@ -10,4 +11,9 @@ interface TranslationDataPersisterInterface
      * @param Product $product
      */
     public function writeProductTranslations(Product $product);
+
+    /**
+     * @param Category $category
+     */
+    public function writeCategoryTranslations(Category $category);
 }
