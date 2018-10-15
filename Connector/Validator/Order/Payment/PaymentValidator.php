e6abe1dc514ea0cc51cd5a889df0219123189e63
Jochen Manz <jochen.manz@gmx.de>
2018-10-15 15:46:45 +0200

    payment validation

--- Connector/Validator/Order/Payment/PaymentValidator.php
+++ Connector/Validator/Order/Payment/PaymentValidator.php
@@ -22,7 +22,7 @@ class PaymentValidator implements ValidatorInterface
      */
     public function validate($object)
     {
-        Assertion::uuid($object->getOrderIdentifer(), null, 'payment.identifier');
+        Assertion::uuid($object->getIdentifier(), null, 'payment.identifier');
         Assertion::float($object->getAmount(), null, 'payment.payment.amount');
         Assertion::uuid($object->getShopIdentifier(), null, 'payment.shopIdentifier');
         Assertion::uuid($object->getCurrencyIdentifier(), null, 'payment.currencyIdentifier');
