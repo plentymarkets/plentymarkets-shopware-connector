f892733f30d1a108eba4ed748ebfdbe413679d54
Florian Ressel <info@k-30.de>
2018-11-20 05:49:44 -0400

    change the cronjob scheduler to avoid errors on parallel running cronjobs (#429)
    * initial commit
    
    * update changelog
    
    * Update CHANGELOG.md

--- Subscriber/CronjobSubscriber.php
+++ Subscriber/CronjobSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace PlentyConnector\Subscriber;
 
+use Doctrine\DBAL\Connection;
 use Enlight\Event\SubscriberInterface;
 use Enlight_Components_Cron_EventArgs as Args;
 use Exception;
@@ -41,18 +42,25 @@ class CronjobSubscriber implements SubscriberInterface
      */
     private $logger;
 
+    /**
+     * @var Connection
+     */
+    private $connection;
+
     public function __construct(
         ConnectorInterface $connector,
         CleanupServiceInterface $cleanupService,
         BacklogServiceInterface $backlogService,
         ServiceBusInterface $serviceBus,
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        Connection $connection
     ) {
         $this->connector = $connector;
         $this->cleanupService = $cleanupService;
         $this->backlogService = $backlogService;
         $this->serviceBus = $serviceBus;
         $this->logger = $logger;
+        $this->connection = $connection;
     }
 
     /**
@@ -74,12 +82,16 @@ class CronjobSubscriber implements SubscriberInterface
      */
     public function onRunCronjobSynchronize(Args $args)
     {
-        try {
-            $this->connector->handle(QueryType::CHANGED);
-        } catch (Exception $exception) {
-            $this->logger->error($exception->getMessage());
-        } catch (Throwable $exception) {
-            $this->logger->error($exception->getMessage());
+        $cronjobState = $this->checkCronjobState(['Shopware_CronJob_PlentyConnector' . PlentyConnector::CRONJOB_BACKLOG]);
+
+        if (!$cronjobState) {
+            try {
+                $this->connector->handle(QueryType::CHANGED);
+            } catch (Exception $exception) {
+                $this->logger->error($exception->getMessage());
+            } catch (Throwable $exception) {
+                $this->logger->error($exception->getMessage());
+            }
         }
 
         $args->setReturn(true);
@@ -94,17 +106,21 @@ class CronjobSubscriber implements SubscriberInterface
      */
     public function onRunCronjobProcessBacklog(Args $args)
     {
-        try {
-            $counter = 0;
-            while ($counter < 200 && $command = $this->backlogService->dequeue()) {
-                ++$counter;
+        $cronjobState = $this->checkCronjobState(['Shopware_CronJob_PlentyConnector' . PlentyConnector::CRONJOB_SYNCHRONIZE]);
 
-                $this->serviceBus->handle($command);
+        if (!$cronjobState) {
+            try {
+                $counter = 0;
+                while ($counter < 200 && $command = $this->backlogService->dequeue()) {
+                    ++$counter;
+
+                    $this->serviceBus->handle($command);
+                }
+            } catch (Exception $exception) {
+                $this->logger->error($exception->getMessage());
+            } catch (Throwable $exception) {
+                $this->logger->error($exception->getMessage());
             }
-        } catch (Exception $exception) {
-            $this->logger->error($exception->getMessage());
-        } catch (Throwable $exception) {
-            $this->logger->error($exception->getMessage());
         }
 
         $args->setReturn(true);
@@ -119,16 +135,42 @@ class CronjobSubscriber implements SubscriberInterface
      */
     public function onRunCronjobCleanup(Args $args)
     {
-        try {
-            $this->cleanupService->cleanup();
-        } catch (Exception $exception) {
-            $this->logger->error($exception->getMessage());
-        } catch (Throwable $exception) {
-            $this->logger->error($exception->getMessage());
+        $cronjobState = $this->checkCronjobState(['Shopware_CronJob_PlentyConnector' . PlentyConnector::CRONJOB_BACKLOG, 'Shopware_CronJob_PlentyConnector' . PlentyConnector::CRONJOB_SYNCHRONIZE]);
+
+        if (!$cronjobState) {
+            try {
+                $this->cleanupService->cleanup();
+            } catch (Exception $exception) {
+                $this->logger->error($exception->getMessage());
+            } catch (Throwable $exception) {
+                $this->logger->error($exception->getMessage());
+            }
         }
 
         $args->setReturn(true);
 
         return true;
+    }
+
+    /**
+     * @param array $cronjobs
+     *
+     * @return bool
+     */
+    private function checkCronjobState($cronjobs = [])
+    {
+        if (empty($cronjobs)) {
+            return false;
+        }
+
+        foreach ($cronjobs as $cronjob) {
+            $state = $this->connection->fetchOne('SELECT active FROM `s_crontab` WHERE action = ?', [$cronjob]);
+
+            if ($state) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
