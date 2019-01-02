6ca00b2a9ff0ba58f8d7898587469b45a21b9f9e
fabeck@arvatis.com <29pfbas11>
2019-01-02 16:59:49 +0100

    cs fix

--- Connector/ValidatorService/ValidatorService.php
+++ Connector/ValidatorService/ValidatorService.php
@@ -4,10 +4,10 @@ namespace SystemConnector\ValidatorService;
 
 use Assert\InvalidArgumentException;
 use DateTimeZone;
+use function is_array;
 use SystemConnector\Validator\ValidatorInterface;
 use SystemConnector\ValidatorService\Exception\InvalidDataException;
 use Traversable;
-use function is_array;
 
 class ValidatorService implements ValidatorServiceInterface
 {
@@ -50,9 +50,8 @@ class ValidatorService implements ValidatorServiceInterface
             });
 
             foreach ($methods as $method) {
-
                 if ($object instanceof DateTimeZone) {
-                  continue;
+                    continue;
                 }
 
                 $result = $object->$method();
