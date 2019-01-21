098cdc8c9cf08b00956789df4c9d43d269c64587
fabeck@arvatis.com <29pfbas11>
2019-01-21 08:33:38 +0100

    deadcode phpdoc

--- Adapter/PlentymarketsAdapter/ReadApi/Item/Variation.php
+++ Adapter/PlentymarketsAdapter/ReadApi/Item/Variation.php
@@ -31,7 +31,7 @@ class Variation extends ApiAbstract
     public function findBy(array $criteria)
     {
         $params = array_merge($criteria, [
-            'with' => implode(',', $this->includes),
+            'with' => implode(',', self::$includes),
         ]);
 
         return iterator_to_array($this->client->getIterator('items/variations', $params));
