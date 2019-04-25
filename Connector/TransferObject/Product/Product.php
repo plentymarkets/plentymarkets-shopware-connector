c2bc24992ebcbd93552cdad83b4e436225c71562
Patryk Fabeck <fabeck@arvatis.com>
2019-04-25 13:38:52 +0200

    multipack fix (#468)
    * multipack fix
    
    * csfixer

--- Connector/TransferObject/Product/Product.php
+++ Connector/TransferObject/Product/Product.php
@@ -16,6 +16,7 @@ use SystemConnector\ValueObject\Translation\Translation;
 class Product extends AbstractTransferObject implements TranslateableInterface, AttributableInterface
 {
     const TYPE = 'Product';
+    const MULTIPACK = 'multiPack';
 
     /**
      * Identifier of the object.
